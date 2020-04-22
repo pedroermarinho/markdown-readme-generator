458dba204b2e9bb968bc8ac433cfcda4a6c08642
Pedro Marinho <pedro.marinho238@gmail>
2020-04-22 16:43:08 -0400

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='readme-generator',
     version='0.1.0',
-    packages=['generator', 'generator.model', 'templates'],
+    packages=setuptools.find_packages(),
     url='https://github.com/pedroermarinho/readme-generator',
     license='MIT',
     author='Pedro Marinho',
     author_email='pedro.marinho238@gmail.com',
-    description='readme generator'
+    description='readme generator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
