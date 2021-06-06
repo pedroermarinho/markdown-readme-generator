8111ffa3442cbd8ccdfb6bd3ddc944b85cfed9b4
Edward Morgan <papaacquah17@gmail.com>
2021-06-06 22:09:15 +0000

    fixing  'charmap' codec can't encode character bug

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
+with open('requirements.txt', encoding='utf-8') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
